a8c406adc6883205a165eece78dcb6d9a756f8db
Jouni Malinen <j@w1.fi>
2017-09-22 12:06:37 +0300

    FT: Do not allow multiple Reassociation Response frames
    The driver is expected to not report a second association event without
    the station having explicitly request a new association. As such, this
    case should not be reachable. However, since reconfiguring the same
    pairwise or group keys to the driver could result in nonce reuse issues,
    be extra careful here and do an additional state check to avoid this
    even if the local driver ends up somehow accepting an unexpected
    Reassociation Response frame.
    
    Bug: 65245581
    Test: WiFi Integration Suite
    Change-Id: Ic951a6fc87a742f7811b44ea0d93e6dae515c9ee
    Merged-In: Ic951a6fc87a742f7811b44ea0d93e6dae515c9ee
    Signed-off-by: Jouni Malinen <j@w1.fi>
    Signed-off-by: Glen Kuhne <kuh@google.com>

--- src/rsn_supp/wpa.c
+++ src/rsn_supp/wpa.c
@@ -2224,6 +2224,9 @@ void wpa_sm_notify_disassoc(struct wpa_sm *sm)
 #ifdef CONFIG_TDLS
 	wpa_tdls_disassoc(sm);
 #endif /* CONFIG_TDLS */
+#ifdef CONFIG_IEEE80211R
+	sm->ft_reassoc_completed = 0;
+#endif /* CONFIG_IEEE80211R */
 }
 
 
