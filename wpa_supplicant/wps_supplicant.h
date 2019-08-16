b18a958c2e1e80bc802ecc298965b2658486737c
Mikael Kanstrup <mikael.kanstrup@sony.com>
2019-08-16 08:50:54 +0200

    WPS: Update MAC address on address changes
    The WPS component keeps a copy of the network interface MAC address.
    When MAC address is changed the WPS copy was not updated so WPS M1
    message contained the old address. Some devices check this field
    and fail connection attempts.
    
    Update the WPS MAC address on interface MAC address changes.
    
    (cherry-picked from 8788a314d7897c90195c6f94810f55b2ba6e7190)
    
    Bug: 140748809
    Test: manually test with some TVs (WFD sink)
    Signed-off-by: Mikael Kanstrup <mikael.kanstrup@sony.com>
    Signed-off-by: Daichi Ueura <daichi.ueura@sony.com>
    Change-Id: Id8b7ec54bdbbf8b18416af6e3063af163344f5b1

--- wpa_supplicant/wps_supplicant.h
+++ wpa_supplicant/wps_supplicant.h
@@ -62,6 +62,7 @@ struct wpabuf * wpas_wps_er_nfc_config_token(struct wpa_supplicant *wpa_s,
 					     int ndef, const char *uuid);
 int wpas_wps_terminate_pending(struct wpa_supplicant *wpa_s);
 void wpas_wps_update_config(struct wpa_supplicant *wpa_s);
+void wpas_wps_update_mac_addr(struct wpa_supplicant *wpa_s);
 struct wpabuf * wpas_wps_network_config_token(struct wpa_supplicant *wpa_s,
 					  int ndef, struct wpa_ssid * ssid);
 struct wpabuf * wpas_wps_nfc_config_token(struct wpa_supplicant *wpa_s,
@@ -154,6 +155,10 @@ static inline int
 wpas_wps_reenable_networks_pending(struct wpa_supplicant *wpa_s)
 {
 	return 0;
+}
+
+static inline void wpas_wps_update_mac_addr(struct wpa_supplicant *wpa_s)
+{
 }
 
 #endif /* CONFIG_WPS */
