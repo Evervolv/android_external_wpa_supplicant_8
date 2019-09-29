a573f4db23a10e76efce298e7f785102ccda56ba
Jimmy Chen <jimmycmchen@google.com>
2019-09-29 15:48:18 +0800

    wifi: uprev ISupplicant to 1.3
    Bug: 141764710
    Test: atest VtsTrebleVendorVintfTest
    Change-Id: Ife894a5ed576e787d712607ecba43eeb96263ed7

--- wpa_supplicant/hidl/1.3/supplicant.h
+++ wpa_supplicant/hidl/1.3/supplicant.h
@@ -13,7 +13,7 @@
 #include <android/hardware/wifi/supplicant/1.0/ISupplicantCallback.h>
 #include <android/hardware/wifi/supplicant/1.0/ISupplicantIface.h>
 #include <android/hardware/wifi/supplicant/1.0/types.h>
-#include <android/hardware/wifi/supplicant/1.2/ISupplicant.h>
+#include <android/hardware/wifi/supplicant/1.3/ISupplicant.h>
 #include <android-base/macros.h>
 #include <hidl/Status.h>
 
@@ -38,7 +38,7 @@ using namespace android::hardware::wifi::supplicant::V1_0;
  * object is used core for global control operations on
  * wpa_supplicant.
  */
-class Supplicant : public V1_2::ISupplicant
+class Supplicant : public V1_3::ISupplicant
 {
 public:
 	Supplicant(struct wpa_global* global);
