e06118e239def4b21b0b8e189efdcd8f840bfccb
Sunil Ravi <sunilravi@google.com>
2021-01-03 08:39:46 -0800

    wifi: Send MBO-OCE association rejection info
    Parse association response for MBO association
    disallowed indication and OCE RSSI based association
    rejection info and send it to framework in association
    rejection event.
    
    Bug: 162542063
    Test: vts test - VtsHalWifiSupplicantV1_4TargetTest
    
    Change-Id: Ie6b9d81491274e06a9521a2e45feb9bf2feabeb5

--- wpa_supplicant/notify.c
+++ wpa_supplicant/notify.c
@@ -149,14 +149,15 @@ void wpas_notify_auth_status_code(struct wpa_supplicant *wpa_s)
 
 
 void wpas_notify_assoc_status_code(struct wpa_supplicant *wpa_s,
-				   const u8 *bssid, u8 timed_out)
+				   const u8 *bssid, u8 timed_out,
+				   const u8 *assoc_resp_ie, size_t assoc_resp_ie_len)
 {
 	if (wpa_s->p2p_mgmt)
 		return;
 
 	wpas_dbus_signal_prop_changed(wpa_s, WPAS_DBUS_PROP_ASSOC_STATUS_CODE);
 
-	wpas_hidl_notify_assoc_reject(wpa_s, bssid, timed_out);
+	wpas_hidl_notify_assoc_reject(wpa_s, bssid, timed_out, assoc_resp_ie, assoc_resp_ie_len);
 }
 
 void wpas_notify_auth_timeout(struct wpa_supplicant *wpa_s) {
